424512744803db31630004b2bc9b30a645b33faa
Chaitanyaraut4545 <chaitanyarout06042005@gmail.com>
2026-09-20 16:24:33 +0530

    Upgrade to Expo SDK 57 with react 19.2.3, react-native 0.86.3 for Expo Go v57 compatibility

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,11 @@
 import Constants from 'expo-constants';
 
-const debuggerHost = Constants.expoConfig?.hostUri || Constants.manifest?.debuggerHost;
-const localhost = debuggerHost ? debuggerHost.split(':')[0] : 'localhost';
+const debuggerHost =
+  Constants.expoConfig?.hostUri ||
+  Constants.manifest2?.extra?.expoClient?.hostUri ||
+  'localhost:5000';
 
-export const API_BASE_URL = `http://${localhost}:5000`;
-export const SOCKET_URL = `http://${localhost}:5000`;
+const host = debuggerHost.split(':')[0];
+
+export const API_BASE_URL = `http://${host}:5000`;
+export const SOCKET_URL = `http://${host}:5000`;
