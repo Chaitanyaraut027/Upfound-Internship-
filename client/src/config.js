0437c66e83dd3d02e79aa279aa58db35521ada2f
Chaitanyaraut4545 <chaitanyarout06042005@gmail.com>
2026-09-20 13:17:38 +0530

    Update config.js with dynamic host IP resolution for Expo Go physical device testing

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,7 @@
-export const API_BASE_URL = 'http://localhost:5000';
-export const SOCKET_URL = 'http://localhost:5000';
+import Constants from 'expo-constants';
+
+const debuggerHost = Constants.expoConfig?.hostUri || Constants.manifest?.debuggerHost;
+const localhost = debuggerHost ? debuggerHost.split(':')[0] : 'localhost';
+
+export const API_BASE_URL = `http://${localhost}:5000`;
+export const SOCKET_URL = `http://${localhost}:5000`;
